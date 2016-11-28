53789575413b481eb07c7e210a461fbced59171a
GernhartReinlunzen <kappenhagenjens@gmail.com>
2016-11-28 11:16:44 +0100

    jhdkasf

--- src/Control/StackHandler.java
+++ src/Control/StackHandler.java
@@ -1,7 +1,31 @@
 package Control;
 
+import Model.Card;
+import Model.CardStack;
+
+import javax.print.attribute.standard.MediaSize;
+
 /**
  * Created by gast06 on 28.11.2016.
  */
 public class StackHandler {
+    private CardStack<Card> NaziStapel;
+    private CardStack<Card> AblageStapel;
+
+
+    public StackHandler(){
+        NaziStapel = new CardStack<Card>();
+        AblageStapel = new CardStack<Card>();
+        for(int i= 0; i<4;i++){
+            for(int j = 0;j<13;j++){
+                NaziStapel.push(new Card(i,j));
+            }
+        }
+    }
+
+    public void karteUmdrehen(){
+        AblageStapel.push(NaziStapel.top());
+        NaziStapel.pop();
+    }
+
 }
--- src/Model/Card.java
+++ src/Model/Card.java
@@ -6,12 +6,12 @@ import java.awt.geom.Rectangle2D;
  * Created by gast06 on 28.11.2016.
  */
 public class Card{
-    private char color;
+    private int color;
     private int worth;
 
     private Rectangle2D.Double rect;
 
-    public Card(char color, int worth){
+    public Card(int color, int worth){
         this.color = color;
         this.worth = worth;
     }
