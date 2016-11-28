786bb140c658f1a4248fad4cccd7e729985a0de6
gast07 <t.kasimay15@gmail.com>
2016-11-28 11:27:27 +0100

    ewr

--- src/Model/Card.java
+++ src/Model/Card.java
@@ -1,45 +1,19 @@
 package Model;
 
-import View.DrawingPanel;
-import View.InteractableObject;
-
-import java.awt.*;
-import java.awt.event.MouseEvent;
 import java.awt.geom.Rectangle2D;
 
 /**
  * Created by gast06 on 28.11.2016.
  */
-public class Card implements InteractableObject{
+public class Card{
     private char color;
     private int worth;
-    private double xPos, yPos, width, height;
 
     private Rectangle2D.Double rect;
 
-    public Card(char color, int worth, double xPos, double yPos, double width, double height){
+    public Card(char color, int worth){
         this.color = color;
         this.worth = worth;
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.width = width;
-        this.height = height;
-        rect = new Rectangle2D.Double(xPos,yPos,width,height);
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void draw(DrawingPanel dp, Graphics2D g2d) {
-
-    }
-
-    @Override
-    public void update(double dt) {
-
     }
 }
 
