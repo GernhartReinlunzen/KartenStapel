7412268957b58979bc2c5d5872183853a00e5d89
GernhartReinlunzen <kappenhagenjens@gmail.com>
2016-11-28 10:38:15 +0100

    jhdkasf

---
+++ src/Model/Card.java
@@ -0,0 +1,7 @@
+package Model;
+
+/**
+ * Created by gast06 on 28.11.2016.
+ */
+public class Card {
+}
---
+++ src/View/MainView.java
@@ -0,0 +1,7 @@
+package View;
+
+/**
+ * Created by gast06 on 28.11.2016.
+ */
+public class MainView {
+}
