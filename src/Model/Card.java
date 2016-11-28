0ff954513b7a2f46c8b4f297eef2454ead0ec62f
gast07 <t.kasimay15@gmail.com>
2016-11-28 11:07:51 +0100

    ewr

--- src/Model/Card.java
+++ src/Model/Card.java
@@ -1,7 +1,29 @@
 package Model;
 
+import java.awt.geom.Rectangle2D;
+
 /**
  * Created by gast06 on 28.11.2016.
  */
 public class Card {
+    private char color;
+    private int worth;
+    private double xPos, yPos, width, height;
+
+    private Rectangle2D.Double rect;
+
+    public Card(char color, int worth, double xPos, double yPos, double width, double height){
+        this.color = color;
+        this.worth = worth;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.width = width;
+        this.height = height;
+        createCard();
+    }
+
+    public void createCard(){
+        rect = new Rectangle2D.Double(color,worth,xPos,yPos,width,height);
+    }
 }
+
--- src/View/MainView.java
+++
@@ -1,7 +0,0 @@
-package View;
-
-/**
- * Created by gast06 on 28.11.2016.
- */
-public class MainView {
-}
